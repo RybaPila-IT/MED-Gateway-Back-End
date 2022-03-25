9a0c4bbcfbc980e88777a0783501d88199c8be02
RybaPila-IT <r.radziukiewicz2@gmail.com>
2022-03-25 12:05:57 +0100

    Add product schema and model
    Model and schema are currently not used.

---
+++ data/schemas/product.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        maxLength: [50, 'Product name must be shorter than 50 characters, got {VALUE} characters instead']
+    },
+    picture: {
+        type: String,
+        required: true,
+        default: ''
+    },
+    short_description: {
+        type: String,
+        required: true,
+        default: ''
+    },
+    full_description: {
+        type: String,
+        required: true,
+        default: ''
+    },
+    usage_description: {
+        type: String,
+        required: true,
+        default: ''
+    }
+});
+
+module.exports = productSchema;
+
+
