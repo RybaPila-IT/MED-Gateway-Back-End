7806466de771be863b0c5c8f11872608dfd70cd4
RybaPila-IT <r.radziukiewicz2@gmail.com>
2022-03-25 11:48:54 +0100

    Add authentication pipeline and get user endpoint
    Implemented authentication pipeline which
    checks the correctness of submitted JWT.
    Added endpoint fetching user data.

---
+++ controllers/users/get.js
@@ -0,0 +1,36 @@
+const httpStatus = require('http-status-codes');
+const chalk = require("chalk");
+
+const User = require('../../data/models/user');
+const authenticateUserMiddlewarePipeline = require('../../middleware/users/authenticate');
+
+const getUserData = (req, res, next) => {
+    const {_id} = req.token;
+    const projection = {
+        password: 0,
+        registered_at: 0,
+        updated_at: 0,
+        __v: 0
+    };
+
+    User
+        .findById(_id, projection)
+        .then(user => {
+            res
+                .status(httpStatus.OK)
+                .json({
+                    ...user['_doc']
+                });
+        })
+        .catch(err => {
+            console.log(chalk.red('error: fetching user model with valid token', err.message));
+            return next(new Error('error: login attempt performed with valid token failed'));
+        })
+}
+
+const getUser = [
+    ...authenticateUserMiddlewarePipeline,
+    getUserData
+];
+
+module.exports = getUser;
---
+++ middleware/users/authenticate.js
@@ -0,0 +1,46 @@
+const httpStatus = require('http-status-codes');
+const jwt = require('jsonwebtoken');
+const chalk = require("chalk");
+
+const jwtSecretKey = 'JWT_SECRET';
+
+const setResponseToNotFound = (req, res, next) => {
+    res.status(httpStatus.NOT_FOUND);
+    return next(new Error('Page not found'));
+}
+
+const requireBearerAuthorization = (req, res, next) => {
+    if (!req.headers.authorization) {
+        console.log(chalk.yellow('warning: attempt to enter protected route without authorization'));
+        return setResponseToNotFound(req, res, next);
+    }
+    if (!req.headers.authorization.startsWith('Bearer')) {
+        console.log(chalk.yellow('warning: wrong authorization method'));
+        return setResponseToNotFound(req, res, next);
+    }
+    next();
+}
+
+const verifyUserToken = (req, res, next) => {
+    // Hacky solution: Authorization string is in the form 'Bearer Token'.
+    // We split the string by an empty space and grab the second
+    // element from received array to fetch the 'Token'.
+    // At index 0, there is just the text 'Bearer'.
+    const token = req.headers.authorization.split(' ')[1];
+    const jwtSecret = process.env[jwtSecretKey];
+    jwt.verify(token, jwtSecret, {}, (err, decoded) => {
+        if (err) {
+            console.log(chalk.red('error: verifying the jwt:', err.message));
+            return setResponseToNotFound(req, res, next);
+        }
+        req['token'] = decoded;
+        next();
+    });
+}
+
+const authenticateUserMiddlewarePipeline = [
+    requireBearerAuthorization,
+    verifyUserToken
+];
+
+module.exports = authenticateUserMiddlewarePipeline;
