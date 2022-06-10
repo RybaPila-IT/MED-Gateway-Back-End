cb3b9dfd9bcc8cfbf2abc54ad175109542d272c4
RybaPila-IT <r.radziukiewicz2@gmail.com>
2022-06-10 10:54:32 +0200

    Implement verification email sending

--- controllers/users/register.js
+++ controllers/users/register.js
@@ -1,8 +1,10 @@
 const httpStatus = require('http-status-codes');
 const chalk = require('chalk');
 
+const {transporter, defaultOptions} = require('../../mail/transporter');
 const registerUserMiddlewarePipeline = require('../../middleware/users/register');
 const User = require('../../data/models/user');
+const Verification = require('../../data/models/verification');
 
 const tryRegisterUser = (req, res, next) => {
     const {
@@ -22,12 +24,9 @@ const tryRegisterUser = (req, res, next) => {
             organization
         })
         .then(createdUser => {
-            res
-                .status(httpStatus.CREATED)
-                .json({
-                    _id: createdUser['_doc']['_id'],
-                    message: 'Your account has been successfully created'
-                });
+            req.user_id = createdUser['doc']['_id'];
+            req.email = createdUser['doc']['email'];
+            next();
         })
         .catch(err => {
             console.log(chalk.red('error: creating the user:', err.message));
@@ -36,9 +35,71 @@ const tryRegisterUser = (req, res, next) => {
         })
 }
 
+
+const generateVerificationEntry = (req, res, next) => {
+    const {user_id} = req;
+
+    Verification
+        .create({
+            user_id
+        })
+        .then(ver => {
+            req.ver_id = ver['doc']['_id'];
+            next();
+        })
+        .catch(err => {
+            console.log(chalk.red('error: creating verification entry:', err.message));
+            next(new Error('error: creating verification entry'));
+        })
+}
+
+
+const sendVerificationEmail = (req, res, next) => {
+
+    const {
+        email: receiver,
+        ver_id: verificationId
+    } = req;
+
+    const link = `http://localhost:5000/api/verify/${verificationId}`;
+
+    const options = {
+        ...defaultOptions,
+        to: receiver,
+        subject: 'Account verification',
+        text: `Welcome to MED-Gateway System!\n\nIn order to verify the account please visit this link: ${link}`
+    };
+
+    transporter
+        .sendMail(options)
+        .then(() => {
+            next();
+        })
+        .catch(err => {
+            console.log(chalk.red('error: sending the verification email', err.message));
+            res
+                .status(httpStatus.INTERNAL_SERVER_ERROR)
+                .json({
+                    message: `Unable to send verification email. Error: ${err.message}`
+                });
+        });
+}
+
+const sendResponse = (req, res) => {
+    res
+        .status(httpStatus.CREATED)
+        .json({
+            _id: req.user_id,
+            message: 'Your account has been created, please verify the account in order to use all functionalities'
+        });
+}
+
 const registerUser = [
     ...registerUserMiddlewarePipeline,
-    tryRegisterUser
+    tryRegisterUser,
+    generateVerificationEntry,
+    sendVerificationEmail,
+    sendResponse
 ];
 
 module.exports = registerUser;
