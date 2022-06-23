d53028710a802deb0eb4d962b3ce89354092df15
RybaPila-IT <r.radziukiewicz2@gmail.com>
2022-06-23 08:38:29 +0200

    Refactor Mongoose connection and server start

---
+++ env/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+    mongoDbUri: 'MONGO_URI',
+    jwtSecret: 'JWT_SECRET',
+    dicomConverterAccessToken: 'DICOM_CONVERTER_ACCESS_TOKEN',
+    fetalNetAccessToken: 'FETAL_NET_ACCESS_TOKEN',
+    babyNetAccessToken: 'BABY_NET_ACCESS_TOKEN',
+    emailUsername: 'EMAIL_USERNAME',
+    emailPassword: 'EMAIL_PASSWORD'
+};
