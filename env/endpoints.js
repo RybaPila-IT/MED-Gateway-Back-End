73cb15b2720c03eae1baa177523705c04b09cd7f
RybaPila-IT <r.radziukiewicz2@gmail.com>
2022-06-29 10:07:31 +0200

    Set production endpoints

--- env/endpoints.js
+++ env/endpoints.js
@@ -11,13 +11,13 @@ const devEndpoints = {
     }
 }
 
-// TODO (radek.r) Set valid production endpoints.
+
 const prodEndpoints = {
-    MedGatewayBackend: 'http://localhost:5000',
-    DicomConverter: 'http://localhost:8000',
+    MedGatewayBackend: 'https://med-gateway-backend.herokuapp.com',
+    DicomConverter: 'https://med-dicom-converter.herokuapp.com',
     Products: {
         // Fetal-Net Service
-        '625576dda784a265d36ff314': 'http://localhost:7000',
+        '625576dda784a265d36ff314': 'https://fetal-net-service-hsyhifhtpq-lm.a.run.app',
         // Baby-Net Service
         '6256a8a79bceb35be10e391e': 'http://localhost:5000'
     }
